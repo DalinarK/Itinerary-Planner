a38e5a4db6ca49224dce83b790105fe0694d0bde
Dustin Dinh <djdinh@gmail.com>
2016-01-28 01:39:56 -0800

    deleted function

--- server.js
+++ server.js
@@ -53,10 +53,7 @@ app.delete('/vacationlist/:id', function(req, res)
     var id = req.params.id;
     console.log(req.body.location);
     db.vacationdb.findAndModify({query: {_id: mongojs.ObjectId(id)},
-    	remove: true}, 
-    	function (err, doc){
-    		res.json(doc);
-    	}
+    remove: true}, 
 	);
 });
 
